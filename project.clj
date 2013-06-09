4e658551416602f99c829439ad39587d785d4cd9
JHE <jan.herich@gmail.com>
2013-06-09 17:03:21 +0200

    Typo fix

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
 (defproject ai-Labs "1.0.0-SNAPSHOT"
-  :description "Playground for various artificial inteligence programs"
+  :description "Playground for various artificial intelligence programs"
   :dependencies [[org.clojure/clojure "1.5.1"]])
